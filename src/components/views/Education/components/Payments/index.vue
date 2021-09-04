a9c80b215366613a4b6f6b6632edb2ab257e248f
Andrey Ivannikov <dronxxx1993@gmail.com>
2021-09-04 12:04:12 +0400

    fixed

--- src/components/views/Education/components/Payments/index.vue
+++ src/components/views/Education/components/Payments/index.vue
@@ -14,13 +14,7 @@
       libero perferendis, dolore a praesentium atque iusto laudantium
       voluptatibus minus saepe?l12
     </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae amet, iure
-      repellat ipsam doloremque reiciendis voluptas? Est sunt qui eligendi,
-      dolorem, distinctio dolor eos incidunt voluptates tempore sed magnam
-      libero perferendis, dolore a praesentium atque iusto laudantium
-      voluptatibus minus saepe?l12
-    </p>
+  
   </div>
 </template>
 
